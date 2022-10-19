8f07b8fd3fc0f70a2e8e749c1cd3447ecece4ed7
Emlyn Bottomley <bottomley.daniel.e@dol.gov>
2022-10-19 15:16:09 -0400

    implement nlx_report_request and nlx_download_report

--- R/get_nlx.R
+++ R/get_nlx.R
@@ -1,6 +1,7 @@
 #' Asynchronous NLX API Wrapper
 #'
-#' `get_nlx()` accesses the asynchronous NLX REST API for downloading large
+#' `get_nlx()` generates a report request from the NLX REST API and downloads it
+#' into a tidy tibble. The asynchronous API is intended for downloading large
 #' quantities of data from the NLX Research Hub (i.e. an entire month of jobs
 #' data from a single state or territory).
 #'
@@ -13,6 +14,9 @@
 #'   one full day beginning on the start_date.Please note that you cannot
 #'   request more than 35 days of data at a time Dates are midnight to midnight,
 #'   so 2021-06-01 to 2021-06-02 is one day of data, for 2021-06-01
+#' @param date_column The date field that start_date and end_date filter by
+#'   Options are created_date, date_compiled, last_updated_date, or
+#'   date_acquired. Defaults to created_date.
 #' @param sleep_time The amount of time in seconds between queries to the NLX
 #'   API. Avoids rate limitations. Defaults to two seconds.
 #' @param silently Provides updates on status on the progress of the report
@@ -32,6 +36,29 @@ get_nlx <- function(state_or_territory,
                     date_column = 'created_date',
                     sleep_time = 2,
                     silently= TRUE) {
+
+
+  report_status_url <- nlx_report_request(state_or_territory = state_or_territory,
+                                          start_date = start_date,
+                                          end_date = end_date,
+                                          date_column = date_column)
+
+
+  report_output_df <- nlx_download_report(report_status_url = report_status_url,
+                                       sleep_time = sleep_time,
+                                       silently = silently)
+
+  return(tibble::as_tibble(report_output_df))
+
+}
+
+
+
+nlx_report_request <- function(state_or_territory,
+                        start_date,
+                        end_date = NULL,
+                        date_column = 'created_date') {
+
 
   ## Error handling
 
@@ -82,23 +109,38 @@ get_nlx <- function(state_or_territory,
     # Start date of the results (inclusive)
     start = start_date,
     end = end_date,
-    date_column = date_column,
     format = 'csv'
   )
 
 
   report_request_response <- httr::POST('https://api.nlxresearchhub.org/api/job_reports/',
                                         httr::add_headers('X-API-KEY' = api_key,
-                                                    'Content-Type' = 'application/json'),
+                                                          'Content-Type' = 'application/json'),
                                         body = query_params,
                                         encode = 'json')
 
   report_request_json <- jsonlite::fromJSON(httr::content(report_request_response, as = "text", encoding = "UTF-8"))
 
-  report_status_url <- report_request_json$data$url
+  return(report_request_json$data$url)
+}
 
-  # wait for the report to complete and query the API until the report is complete
-  # Sleep for 2 seconds between requests to avoid rate limitations
+
+
+nlx_download_report <- function(report_status_url,
+                                sleep_time = 2,
+                                silently = TRUE) {
+
+
+
+
+  if (Sys.getenv('NLX_API_KEY') != '') {
+    api_key <- Sys.getenv('NLX_API_KEY')
+  } else if (is.null(key)) {
+    stop('An NLX API key is required. If you have not received an API key but
+           expect to have received one, please reach out to admin@nlxresearchhub.org.,
+           and then supply the key to the `nlx_api_key` function to use it throughout your tidynlx sessions.')
+  }
+
   not_done <- TRUE
 
   while (not_done) {
@@ -107,7 +149,7 @@ get_nlx <- function(state_or_territory,
 
     report_status_response <- httr::GET(report_status_url,
                                         httr::add_headers('X-API-KEY' = api_key,
-                                                    'Content-Type' = 'application/json'))
+                                                          'Content-Type' = 'application/json'))
 
     report_status_json <-jsonlite::fromJSON(httr::content(report_status_response, as="text", encoding = "UTF-8"))
 
@@ -136,7 +178,4 @@ get_nlx <- function(state_or_territory,
   report_output_df <- readr::read_csv(report_status_json$data$resource$link)
 
   return(tibble::as_tibble(report_output_df))
-
 }
-
-
